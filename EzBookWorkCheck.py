1518e72e511e4c6ccf59becde797aab02a390c6d
Virtual <60713602+Virtual065@users.noreply.github.com>
2023-11-13 14:27:30 +0000

    Update EzBookWorkCheck.py

--- EzBookWorkCheck.py
+++ EzBookWorkCheck.py
@@ -1,1 +1,24 @@
+file = open("main.txt", "a")
 
+counter = 1
+number = 1
+
+while True:
+
+    text = input(f"{number}{chr(96 + counter)} = ")
+    
+    if text.lower() == "quit":
+        break
+    
+    if text.lower() == "next":
+
+        number += 1
+        counter = 1
+        file.write("\n")
+        continue
+    
+    file.write(f"{number}{chr(96 + counter)} = {text}\n")
+    
+    counter += 1
+
+file.close()
